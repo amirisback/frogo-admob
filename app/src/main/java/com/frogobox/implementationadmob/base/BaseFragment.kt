743c2a4abc40dfe247b337728d1af208f86235a2
amirisback <faisalamircs@gmail.com>
2019-11-25 16:57:10 +0700

    update: BaseFragment.kt

--- app/src/main/java/com/frogobox/implementationadmob/base/BaseFragment.kt
+++ app/src/main/java/com/frogobox/implementationadmob/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.frogobox.implementationadmob.base
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 /**
@@ -21,6 +22,12 @@ import androidx.fragment.app.Fragment
  */
 class BaseFragment : Fragment() {
 
+    lateinit var mActivity: BaseActivity
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mActivity = (activity as BaseActivity)
+    }
 
 
 }
