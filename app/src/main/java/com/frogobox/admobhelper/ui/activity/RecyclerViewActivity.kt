4891875a9b76b7877aa070b341bf6bbcee4ffa94
amirisback <faisalamircs@gmail.com>
2019-11-29 22:30:21 +0700

    add: RecyclerView Adapter With Ads Banner

--- app/src/main/java/com/frogobox/admobhelper/ui/activity/RecyclerViewActivity.kt
+++ app/src/main/java/com/frogobox/admobhelper/ui/activity/RecyclerViewActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.frogobox.admobhelper.R
 import com.frogobox.admobhelper.base.adapter.BaseViewListener
+import com.frogobox.admobhelper.base.admob.BaseAdmobRecyclerActivity
 import com.frogobox.admobhelper.base.ui.BaseActivity
+import com.frogobox.admobhelper.helper.AdmobHelper.RecyclerView.loadRecyclerBannerAds
 import com.frogobox.admobhelper.model.Fruit
 import com.frogobox.admobhelper.ui.adapter.FruitAdapter
+import com.google.android.gms.ads.AdView
 import kotlinx.android.synthetic.main.activity_recycler_view.*
 
-class RecyclerViewActivity : BaseActivity(), BaseViewListener<Fruit> {
-
+class RecyclerViewActivity : BaseAdmobRecyclerActivity(), BaseViewListener<Any> {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
@@ -18,30 +21,33 @@ class RecyclerViewActivity : BaseActivity(), BaseViewListener<Fruit> {
         setupRecyclerView()
     }
 
-    private fun arrayFruit() : MutableList<Fruit> {
-        val dataList = mutableListOf<Fruit>()
-        dataList.add(Fruit("Apple", "Fresh"))
-        dataList.add(Fruit("Grape", "Fresh"))
-        dataList.add(Fruit("Water Melon", "Fresh"))
-        dataList.add(Fruit("Blackberry", "Fresh"))
-        dataList.add(Fruit("Banana", "Fresh"))
-        return dataList
+    private fun arrayFruit() : MutableList<Any> {
+        arrayRecyclerView.add(Fruit("Apple", "Fresh"))
+        arrayRecyclerView.add(Fruit("Grape", "Fresh"))
+        arrayRecyclerView.add(Fruit("Water Melon", "Fresh"))
+        arrayRecyclerView.add(Fruit("Blackberry", "Fresh"))
+        arrayRecyclerView.add(Fruit("Banana", "Fresh"))
+        return arrayRecyclerView
+    }
+
+    private fun setupAdapter() : FruitAdapter{
+        val adapter = FruitAdapter()
+        adapter.setupRequirement(this, arrayFruit(), R.layout.content_item_fruit)
+        loadRecyclerBannerAds(this, arrayFruit())
+        return adapter
     }
 
     private fun setupRecyclerView(){
-        val adapter = FruitAdapter()
-        adapter.setRecyclerViewLayout(this, R.layout.content_item_fruit)
-        adapter.setRecyclerViewListener(this)
-        adapter.setRecyclerViewData(arrayFruit())
-        recycler_view.adapter = adapter
         recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        recycler_view.adapter = setupAdapter()
     }
 
-    override fun onItemClicked(data: Fruit) {
+    override fun onItemClicked(data: Any) {
 
     }
 
-    override fun onItemLongClicked(data: Fruit) {
+    override fun onItemLongClicked(data: Any) {
 
     }
+    
 }
