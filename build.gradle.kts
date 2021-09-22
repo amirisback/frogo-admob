ca0d6bedbcbfac0bdf37625f974212842b1415c6
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-22 14:04:31 +0700

    update: build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,6 @@
 
 buildscript {
 
-    val compose_version by extra("1.0.1")
     repositories {
         google()
         mavenCentral()
@@ -10,7 +9,7 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Dependency.KOTLIN_VERSION}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
