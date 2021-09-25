49c0bfa9dc20270e0bd755f021e16f5a69d990ef
Muhammad Faisal Amir <faisalamircs@gmail.com>
2021-09-25 14:00:11 +0700

    update: reimport unused res

--- frogoadmob/src/main/java/com/frogobox/admob/core/FrogoAdmobCompose.kt
+++ frogoadmob/src/main/java/com/frogobox/admob/core/FrogoAdmobCompose.kt
@@ -3,7 +3,6 @@ package com.frogobox.admob.core
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
