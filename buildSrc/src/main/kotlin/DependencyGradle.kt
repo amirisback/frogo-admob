0f8e869031dc9e564b3a058bdc1523be4b3790aa
Faisal Amir <faisalamircs@gmail.com>
2022-04-17 15:18:30 +0700

    Update DependencyGradle.kt

--- buildSrc/src/main/kotlin/DependencyGradle.kt
+++ buildSrc/src/main/kotlin/DependencyGradle.kt
@@ -16,10 +16,10 @@ object DependencyGradle {
     const val KOTLIN_VERSION = Version.JetBrains.kotlin
     const val COMPOSE_VERSION = Version.Androidx.compose
 
-    const val FROGO_SDK = "1.0.2"
+    const val FROGO_SDK = "1.0.3"
     const val FROGO_UI = "1.0.0"
-    const val FROGO_CONSUME_API = "2.2.1"
-    const val FROGO_RECYCLER_VIEW = "4.1.1"
+    const val FROGO_CONSUME_API = "2.2.2"
+    const val FROGO_RECYCLER_VIEW = "4.1.3"
 
     const val FROGO_PATH_ADMOB = ":frogoadmob"
 
